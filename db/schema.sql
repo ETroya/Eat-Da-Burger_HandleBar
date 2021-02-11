a0cce92429610bd6133bab9588e3bee0143d3466
Edwin Troya <edtroyajr@gmail.com>
2021-02-10 20:28:26 -0800

    database working

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int AUTO_INCREMENT PRIMARY KEY (id),
+	burger_name varchar(100) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	
+);
---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('CheeseBurger');
+INSERT INTO burgers (burger_name) VALUES ('Jucy Lucy');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Vegan Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Protein Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Chicken Sandwich');
+
